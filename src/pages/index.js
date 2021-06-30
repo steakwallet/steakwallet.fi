42db164f8a79ea279b570f9ab623d24787437a2d
SonnyCastro <sonnycastro0606@gmail.com>
2021-06-30 14:56:44 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import { useGLTF, OrbitControls } from "@react-three/drei";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { useSpring, animated, config } from "@react-spring/three";
-import logo from "../images/logo.svg";
+import logo from "../images/newLogo.svg";
+import appleBtn from "../images/appleBtn.svg";
+import androidBtn from "../images/android_coming_soon.svg";
 import flexaReg from "../fonts/GT-Flexa-Standard-Regular.woff2";
 import flexaBold from "../fonts/GT-Flexa-Standard-Bold.woff2";
 import multichain from "../images/multichain.jpg";
@@ -12,6 +14,8 @@ import easy from "../images/easy.jpg";
 import keys from "../images/keys.jpg";
 import SEO from "../components/seo";
 import ogImg from "../images/ogimage.png";
+import cta from "../images/cta.svg";
+import ape from "../images/ape.svg";
 
 const lg = "684px";
 const xl = "1680";
@@ -45,7 +49,7 @@ const GlobalStyle = createGlobalStyle`
   body{
     margin: 0;
     padding: 0;
-    background: #000;
+    background: white;
     font-family: "flexa", "Helvetica Neue", Helvetica, arial, sans;
     font-size: 16px;
     line-height: 16px;
@@ -87,12 +91,12 @@ const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  color: #fff;
+  color: black;
   z-index: 3;
   a {
     margin-left: 1rem;
     font-size: 0.75rem;
-    color: #fff;
+    color: black;
     text-decoration: none;
     pointer-events: auto;
     &:hover {
@@ -103,12 +107,17 @@ const NavBar = styled.div`
       font-size: 1rem;
     }
   }
+  .hide {
+    @media (max-width: ${lg}) {
+      display: none;
+    }
+  }
 `;
 const LogoImg = styled.img`
   width: 112px;
   height: auto;
   @media (min-width: ${lg}) {
-    width: 140px;
+    width: 170px;
   }
 `;
 
@@ -117,7 +126,7 @@ const HeroContent = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #fff;
+  color: black;
   position: relative;
   width: 100vw;
   height: 100vh;
@@ -172,33 +181,64 @@ const CTA = styled.a`
   }
 `;
 
+const AppleBtn = styled.img`
+  width: 218px;
+  height: auto;
+  cursor: pointer;
+  @media (min-width: ${lg}) {
+    width: 218px;
+  }
+`;
+
+const AndroidBtn = styled.img`
+  width: 218px;
+  height: auto;
+  margin-top: 1.5rem;
+  cursor: pointer;
+  @media (min-width: ${lg}) {
+    width: 218px;
+  }
+`;
+
+const Ape = styled.img`
+  width: 218px;
+  height: auto;
+  margin-top: 1.5rem;
+  cursor: pointer;
+  position: relative;
+  bottom: 5rem;
+  @media (min-width: ${lg}) {
+    width: 218px;
+  }
+`;
+
 const FeatureContent = styled.div`
-  padding: 2rem;
+  // padding: 2rem;
   display: flex;
   flex-direction: column;
+  align-items: center;
   position: relative;
   max-width: 1280px;
   margin: 0 auto 4rem auto;
-  @media (min-width: ${lg}) {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    column-gap: 2rem;
-  }
 `;
+
 const FeatureItem = styled.div`
   border-radius: 2rem;
-  background: #1c1c1e;
-  border: 1px solid #1c1c1e;
   color: #fff;
   overflow: hidden;
   text-align: center;
   margin-bottom: 1rem;
+  max-width: 817px;
+  max-height: 1188px;
   div {
     padding: 2rem;
   }
   img {
-    width: 100%;
+    width: 95%;
     height: auto;
+  }
+  @media (min-width: ${lg}) {
+    width: 45%;
   }
   h3 {
     font-weight: bold;
@@ -212,12 +252,28 @@ const FeatureItem = styled.div`
 `;
 
 const Footer = styled.footer`
-  color: #909090;
+  color: black;
   padding: 3rem;
   text-align: center;
   display: block;
   margin: 0 auto;
   position: relative;
+  // bottom: 4rem;
+
+  div {
+    display: flex;
+    flex-direction:column;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+    pointer-events: auto;
+    padding-top:.5rem;
+    &:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
 `;
 
 function Steak(props) {
@@ -273,7 +329,7 @@ const IndexPage = () => {
         description="Steakwallet is the easiest and safest way to steak your crypto assets across blockchains."
         image={"https://thesteakwallet.com" + ogImg}
       />
-      <SpaceSteak>
+      {/* <SpaceSteak>
         <Canvas gl={{ alpha: false }} shadows dpr={[1, 2]}>
           <color attach="background" args={["black"]} />
           <ambientLight intensity={1.5} />
@@ -288,39 +344,52 @@ const IndexPage = () => {
             enablePan={false}
           />
         </Canvas>
-      </SpaceSteak>
+      </SpaceSteak> */}
       <Main>
         <NavBar>
           <LogoImg src={logo} alt="Steakwallet" />
           <nav>
-            <a href="https://discord.gg/ZtdgjSEnRy">Discord</a>
+            <a className="hide" href="#">
+              Changelog
+            </a>
+            <a className="hide" href="#">
+              Support
+            </a>
+            <a className="hide" href="#">
+              LitePaper
+            </a>
             <a href="https://twitter.com/steakwallet">Twitter</a>
+            <a href="https://discord.gg/ZtdgjSEnRy">Discord</a>
           </nav>
         </NavBar>
         <HeroContent>
-          <h1>Steaking Made Simple</h1>
+          <h1>The easiest way to steak</h1>
           <p>
-            Steakwallet is the easiest and safest way to steak your crypto
-            assets across blockchains. Start earning interest on your crypto in
-            seconds.
+            Steakwallet is a multi-chain wallet that makes staking your crypto
+            as easy as possible. Start earning interest on your coins in
+            seconds!
           </p>
-          <CTA href="https://form.typeform.com/to/Qemm4FMQ">
+          {/* <CTA href="https://form.typeform.com/to/Qemm4FMQ">
             Sign up for early access
-          </CTA>
+          </CTA> */}
+          <a href="https://form.typeform.com/to/Qemm4FMQ" target="_blank">
+            <AppleBtn src={appleBtn} alt="Download app btn" />
+          </a>
+          <AndroidBtn src={androidBtn} alt="Android coming soon" />
         </HeroContent>
         <FeatureContent>
           <FeatureItem>
-            <img src={multichain} alt="Multi-chain" />
-            <div>
+            <img src={cta} alt="Multi-chain" />
+            {/* <div>
               <h3>Multi-Chain Support</h3>
               <p>
                 Steakwallet is multi-chain, so you don’t need a different wallet
                 for each blockchain. We’re adding support for new tokens and
                 chains fast!
               </p>
-            </div>
+            </div> */}
           </FeatureItem>
-          <FeatureItem>
+          {/* <FeatureItem>
             <img src={easy} alt="Easy steaking" />
             <div>
               <h3>The Easiest Steaking Flow</h3>
@@ -343,9 +412,17 @@ const IndexPage = () => {
                 and you are in full control of your crypto.
               </p>
             </div>
-          </FeatureItem>
+          </FeatureItem> */}
+          {/* <Ape src={ape} alt="Apes together strong" /> */}
         </FeatureContent>
-        <Footer>© Steakwallet 2021</Footer>
+        <Footer>
+          <Ape src={ape} alt="Apes together strong" />
+          <div>
+            <p>© 2021 Steakwallet LLC</p>
+            <a href="#">Terms of use</a>
+            <a href="#">Privacy Policy</a>
+          </div>
+        </Footer>
       </Main>
     </>
   );
